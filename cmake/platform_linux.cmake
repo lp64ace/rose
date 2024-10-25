929f878551af6cacc59ceb85fa997fde4f2266fa
lp64ace <akomikron@gmail.com>
2024-10-25 23:33:40 +0300

    Updated cmake requirements for linux systems

---
+++ cmake/platform_linux.cmake
@@ -0,0 +1,22 @@
+add_definitions(-DLINUX)
+
+# Path to a locally compiled libraries.
+set(LIBDIR_NAME ${CMAKE_SYSTEM_NAME}_${CMAKE_SYSTEM_PROCESSOR})
+string(TOLOWER ${LIBDIR_NAME} LIBDIR_NAME)
+set(LIBDIR_NATIVE_ABI ${CMAKE_SOURCE_DIR}/cmake/lib/${LIBDIR_NAME})
+
+if(EXISTS ${LIBDIR_NATIVE_ABI})
+    set(LIBDIR ${LIBDIR_NATIVE_ABI})
+	message(STATUS "[${CMAKE_SYSTEM_NAME}_${CMAKE_SYSTEM_PROCESSOR}] Linux detected.")
+else()
+	message(FATAL_ERROR "Unable to find LIBDIR_NATIVE_ABI: ${LIBDIR_NATIVE_ABI}.")
+endif()
+
+# -----------------------------------------------------------------------------
+# Pre-Built Libraries
+
+find_package(X11 REQUIRED)
+
+if (NOT X11_FOUND)
+    message(FATAL_ERROR "X11 not found. Install it using `sudo apt-get install libx11-dev`")
+endif()
