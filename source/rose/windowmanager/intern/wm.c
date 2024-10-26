0af354f366e0385271b48bc061a7f257129603f8
Jim <akomikron@gmail.com>
2024-10-26 21:55:51 +0300

    Made sample window using TinyWindow instead of GHOST

--- source/rose/windowmanager/intern/wm.c
+++ source/rose/windowmanager/intern/wm.c
@@ -4,25 +4,19 @@
 
 #include "WM_api.h"
 
-#include <glib.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "wtk_api.h"
+
+static WTKWindowManager *manager = NULL;
+static WTKWindow *window = NULL;
 
 /* -------------------------------------------------------------------- */
 /** \name Init & Exit Methods
  * \{ */
 
-static struct WTKPlatform *g_platform = NULL;
-static struct WTKWindow *g_window = NULL;
-
 void WM_init() {
-	g_platform = WTK_platform_init();
-
-	g_window = WTK_window_spawn(g_platform, "Rose", 0xffff, 0xffff, 1024, 600);
-	if (g_window) {
-		WTK_window_show_ex(g_window, WTK_SW_DEFAULT);
-	}
-
+	manager = WTK_window_manager_new();
+	window = WTK_create_window(manager, "Rose", 1280, 800);
+	
 	KER_idtype_init();
 	
 	Main *main = KER_main_new();
@@ -31,18 +25,18 @@ void WM_init() {
 }
 
 void WM_main() {
-	/** This will eventually be called forever until the application has to exit. */
-	if (!WTK_window_is_valid(g_platform, g_window)) {
+	if(WTK_window_should_close(window)) {
+		WTK_window_free(manager, window);
 		WM_exit();
 	}
 	
-	WTK_platform_poll(g_platform);
+	WTK_window_manager_poll(manager);
 }
 
 void WM_exit() {
 	KER_rose_globals_clear();
 	
-	WTK_platform_exit(g_platform);
+	WTK_window_manager_free(manager);
 
 	exit(0);
 }
