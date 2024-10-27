965ec111199401f27737cad7d4d4eb1f911d570b
Jim <akomikron@gmail.com>
2024-10-27 20:32:04 +0200

    Added data_to_c in cmake

--- cmake/cfg.cmake
+++ cmake/cfg.cmake
@@ -34,3 +34,61 @@ function(detect_endianess)
 		add_definitions(-D__BIG_ENDIAN__)
 	endif()
 endfunction()
+
+# -----------------------------------------------------------------------------
+# Data to Code
+
+function(data_to_c
+	file_from
+	file_to
+	list_to_add
+)
+	list(APPEND ${list_to_add} ${file_to})
+	set(${list_to_add} ${${list_to_add}} PARENT_SCOPE)
+	
+	get_filename_component(_file_to_path ${file_to} PATH)
+	
+	add_custom_command(
+		OUTPUT
+			${file_to}
+		COMMAND
+			${CMAKE_COMMAND} -E make_directory ${_file_to_path}
+		COMMAND
+			"$<TARGET_FILE:datatoc>" --src ${file_from} --bin ${file_to}
+		DEPENDS
+			${file_from}
+			rose::source::datatoc
+	)
+	
+	set_source_files_properties(${file_to} PROPERTIES GENERATED TRUE)
+endfunction()
+
+function(data_to_c_simple
+	file_from
+	list_to_add
+)
+	# remove ../'s
+	get_filename_component(_file_from ${CMAKE_CURRENT_SOURCE_DIR}/${file_from}   REALPATH)
+	get_filename_component(_file_to   ${CMAKE_CURRENT_BINARY_DIR}/${file_from}.c REALPATH)
+
+	list(APPEND ${list_to_add} ${_file_to})
+	source_group(Generated FILES ${_file_to})
+	list(APPEND ${list_to_add} ${file_from})
+	set(${list_to_add} ${${list_to_add}} PARENT_SCOPE)
+
+	get_filename_component(_file_to_path ${_file_to} PATH)
+	
+	add_custom_command(
+		OUTPUT
+			${_file_to}
+		COMMAND
+			${CMAKE_COMMAND} -E make_directory ${_file_to_path}
+		COMMAND
+			"$<TARGET_FILE:datatoc>" --src ${_file_from} --bin ${_file_to}
+		DEPENDS
+			${_file_from}
+			rose::source::datatoc
+	)
+
+	set_source_files_properties(${_file_to} PROPERTIES GENERATED TRUE)
+endfunction()
