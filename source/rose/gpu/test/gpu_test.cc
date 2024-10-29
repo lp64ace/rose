672acd5c76f6a6b302a1386be79d8a029b1e50d5
Jim <akomikron@gmail.com>
2024-10-29 15:56:51 +0200

    Updated tests

--- source/rose/gpu/test/gpu_test.cc
+++ source/rose/gpu/test/gpu_test.cc
@@ -6,18 +6,24 @@ namespace rose::gpu {
 
 void GPUTest::SetUp() {
 	manager = WTK_window_manager_new();
-	ASSERT_NE(manager, nullptr);
+	EXPECT_NE(manager, nullptr);
 	window = WTK_create_window(manager, "gpu::test", 800, 600);
-	ASSERT_NE(window, nullptr);
+	EXPECT_NE(window, nullptr);
 
 	WTK_window_make_context_current(window);
 	context = GPU_context_create(window, NULL);
-	ASSERT_NE(context, nullptr);
+	EXPECT_NE(context, nullptr);
 }
 void GPUTest::TearDown() {
-	GPU_context_discard(context);
-	WTK_window_free(manager, window);
-	WTK_window_manager_free(manager);
+	if (context) {
+		GPU_context_discard(context);
+	}
+	if (window) {
+		WTK_window_free(manager, window);
+	}
+	if (manager) {
+		WTK_window_manager_free(manager);
+	}
 }
 
 } // namespace rose::gpu
