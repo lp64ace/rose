3b123270da4dbab8355a701f376f272eadc61e3b
Jim <akomikron@gmail.com>
2024-10-29 15:52:17 +0200

    Added assert for context creation

--- source/rose/gpu/test/gpu_test.cc
+++ source/rose/gpu/test/gpu_test.cc
@@ -7,11 +7,12 @@ namespace rose::gpu {
 void GPUTest::SetUp() {
 	manager = WTK_window_manager_new();
 	ASSERT_NE(manager, nullptr);
-	window = WTK_create_window(manager, "GPUTEST", 800, 600);
+	window = WTK_create_window(manager, "gpu::test", 800, 600);
 	ASSERT_NE(window, nullptr);
 
 	WTK_window_make_context_current(window);
 	context = GPU_context_create(window, NULL);
+	ASSERT_NE(context, nullptr);
 }
 void GPUTest::TearDown() {
 	GPU_context_discard(context);
