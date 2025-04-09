be038117ad809261463bdac2ec1282b9a5a2b2c0
unknown <lp64ace>
2025-04-09 03:12:42 +0300

    Added ViewLayer and Collection types

--- source/rose/makedna/DNA_collection_types.h
+++ source/rose/makedna/DNA_collection_types.h
@@ -21,15 +21,30 @@ typedef struct CollectionChild {
 typedef struct Collection {
     ID id;
 
+    int flag;
+
     /**
      * This does not store raw objects, it stores #CollectionObject datablocks, 
      * since each #Object belongs to its respective database, and therefore the #Link is already used.
      * \note ID datablocks in general are owned by #Main, and cannot be shared between different #ListBase containers.
      */
     ListBase objects;
+    ListBase object_cache;
+    ListBase object_cache_instanced;
     /** Similar with the #objects ListBase this only stores #CollectionChild datablocks. */
     ListBase children;
+    ListBase parents;
 } Collection;
+
+enum {
+	COLLECTION_IS_MASTER = (1 << 0),
+	COLLECTION_HIDE_VIEWPORT = (1 << 1), /** This object should be disabled for viewport. */
+	COLLECTION_HIDE_SELECT = (1 << 2),	 /** This object is not selectable in viewport. */
+	COLLECTION_HIDE_RENDER = (1 << 3),	 /** This object should be disabled for renders. */
+
+	COLLECTION_HAS_OBJECT_CACHE = (1 << 30),
+	COLLECTION_HAS_OBJECT_CACHE_INSTANCED = (1 << 31),
+};
 
 #ifdef __cplusplus
 }
--- source/rose/makedna/DNA_layer_types.h
+++ source/rose/makedna/DNA_layer_types.h
@@ -3,18 +3,92 @@
 
 #include "DNA_listbase.h"
 
+struct Object;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+typedef struct Base {
+    struct Base *prev, *next;
+
+    int flag;
+	int flag_collection;
+
+    unsigned int local_view_bits;
+	unsigned int local_collections_bits;
+
+    struct Object *object;
+} Base;
+
+/** #Base->flag */
+enum {
+    BASE_SELECTED = (1 << 0),
+    BASE_HIDDEN = (1 << 1),
+
+    BASE_SELECTABLE = (1 << 2),
+    BASE_VISIBLE_VIEWLAYER = (1 << 3),
+    BASE_VISIBLE_DEPSGRAPH = (1 << 4),
+    BASE_ENABLED_VIEWPORT = (1 << 5),
+    BASE_ENABLED_RENDER = (1 << 6),
+};
+
+typedef struct LayerCollection {
+	struct LayerCollection *prev, *next;
+	struct Collection *collection;
+
+    int flag;
+	int runtime_flag;
+
+    unsigned int local_collections_bits;
+
+    /** Synced with collection->children. */
+    ListBase layer_collections;
+} LayerCollection;
+
+/** #LayerCollection->flag */
+enum {
+    LAYER_COLLECTION_EXCLUDE = (1 << 0),
+    LAYER_COLLECTION_HIDE = (1 << 1),
+
+	LAYER_COLLECTION_PREVIOUSLY_EXCLUDED = (1 << 8),
+};
+
+/** #LayerCollection->runtime_flag */
+enum {
+	LAYER_COLLECTION_HAS_OBJECTS = (1 << 0),
+	LAYER_COLLECTION_HIDE_VIEWPORT = (1 << 2),
+	LAYER_COLLECTION_VISIBLE_VIEW_LAYER = (1 << 3),
+};
 
 typedef struct ViewLayer {
     struct ViewLayer *prev, *next;
 
+    int flag;
+
     char name[64];
 
+    struct Base *active;
+
+    ListBase bases;
     ListBase drawdata;
+	struct Base **object_bases_array;
+	struct GHash *object_bases_hash;
+
+    /**
+     * A view layer has one top level layer collection, because a scene has only one top level collection. 
+     * The layer_collections list always contains a single element. 
+     * 
+     * \note ListBase is convenient when applying functions to all layer collections recursively.
+     */
+	ListBase layer_collections;
+    struct LayerCollection *active_collection;
 } ViewLayer;
+
+/** #ViewLayer->flag */
+enum {
+    VIEW_LAYER_RENDER = (1 << 0),
+};
 
 #ifdef __cplusplus
 }
