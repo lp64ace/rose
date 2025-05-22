a8dd0d5cbf97af9b970ab5fdb94b25c551675247
lp64ace <akomikron@gmail.com>
2025-05-23 00:03:45 +0300

    Updated test suite port

--- intern/network/test/address.cc
+++ intern/network/test/address.cc
@@ -6,7 +6,7 @@ namespace {
 
 TEST(Address, Get) {
 	NET_init();
-	NetAddress *addr = NET_address_new_ex("127.0.0.1", "https", AF_UNSPEC, SOCK_STREAM);
+	NetAddress *addr = NET_address_new_ex("127.0.0.1", "6969", AF_UNSPEC, SOCK_STREAM);
 	ASSERT_NE(addr, nullptr);
 
 	int length = 0;
