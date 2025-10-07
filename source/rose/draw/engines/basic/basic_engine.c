7e83ce8f7af9a1dfa31944476450270784480ca8
lp64ace <akomikron@gmail.com>
2025-10-07 21:26:49 +0300

    Fixed basic engine crash

--- source/rose/draw/engines/basic/basic_engine.c
+++ source/rose/draw/engines/basic/basic_engine.c
@@ -7,6 +7,22 @@
 #include "basic_engine.h"
 
 /* -------------------------------------------------------------------- */
+/** \name Basic Draw Engine Types
+ * \{ */
+
+typedef struct DRWBasicData {
+	struct ViewportEngineData *prev, *next;
+
+	void *engine;
+	DRWViewportEmptyList *fbl;
+	DRWViewportEmptyList *txl;
+	DRWViewportEmptyList *psl;
+	DRWViewportEmptyList *stl;
+} DRWBasicData;
+
+/** \} */
+
+/* -------------------------------------------------------------------- */
 /** \name Basic Draw Engine Cache
  * \{ */
 
@@ -19,10 +35,12 @@ ROSE_STATIC void basic_cache_populate(void *vdata, struct Object *object) {
 /** \name Basic Draw Engine Type Definition
  * \{ */
 
+static const DrawEngineDataSize draw_basic_engine_data_size = DRW_VIEWPORT_DATA_SIZE(DRWBasicData);
+
 DrawEngineType draw_engine_basic_type = {
 	.name = "ROSE_BASIC",
 
-	.vdata_size = NULL,
+	.vdata_size = &draw_basic_engine_data_size,
 
 	.cache_init = NULL,
 	.cache_populate = NULL,
