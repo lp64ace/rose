3916cd61f3ed6fcb3cb6f4be951439c3e4c6b9ae
lp64ace <akomikron@gmail.com>
2026-01-04 08:16:03 +0200

    Added comments

--- source/rose/draw/engines/alice/alice_modifier.c
+++ source/rose/draw/engines/alice/alice_modifier.c
@@ -28,6 +28,12 @@ GPUUniformBuf *DRW_alice_defgroup_ubo(struct Object *object, struct ModifierData
     ArmatureModifierData *amd = (ArmatureModifierData *)md;
     ROSE_assert((md->type == MODIFIER_TYPE_ARMATURE) && (md->flag & MODIFIER_DEVICE_ONLY) != 0);
 
+    /**
+     * We dispatch this every time since this cannot be addead to the depsgraph 
+     * to handle updates only.
+     * 
+     * \note This is intended since this is the purpose of device modifiers (always running).
+     */
     extract_matrices(amd->object, object, object->data, add->defgroup);
 
     return add->defgroup;
