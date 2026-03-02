a1585dec3d1dedd2ea3c7618e5ddee3067569c89
lp64ace <akomikron@gmail.com>
2026-03-02 15:55:56 +0200

    Removed unused code

--- source/rose/editors/space_statusbar/space_statusbar.c
+++ source/rose/editors/space_statusbar/space_statusbar.c
@@ -70,8 +70,6 @@ ROSE_STATIC void statusbar_header_region_layout(rContext *C, ARegion *region) {
 		uiLayout *layout = UI_layout_row(root, PIXELSIZE);
 
 		uiDefBut(block, UI_BTYPE_TEXT, "", 0, 0, 3 * UI_UNIT_X, UI_UNIT_Y, &window->runtime.last.frames_per_second, UI_POINTER_FLT, 0, FLT_MAX, UI_BUT_TEXT_LEFT);
-		// uiDefBut(block, UI_BTYPE_TEXT, "[%.*fms, ", 0, 0, 3 * UI_UNIT_X, UI_UNIT_Y, &window->runtime.last.frame_min_time, UI_POINTER_FLT, 0, FLT_MAX, UI_BUT_TEXT_LEFT | UI_BUT_TEXT_FORMAT);
-		// uiDefBut(block, UI_BTYPE_TEXT, "%.*fms]  ", 0, 0, 3 * UI_UNIT_X, UI_UNIT_Y, &window->runtime.last.frame_max_time, UI_POINTER_FLT, 0, FLT_MAX, UI_BUT_TEXT_LEFT | UI_BUT_TEXT_FORMAT);
 		
 		UI_block_end(C, block);
 	}
